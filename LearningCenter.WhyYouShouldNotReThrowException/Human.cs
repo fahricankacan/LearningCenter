3b62b47f71de70d0af6d64aeaf985a8178b7edd5
Can Köseoğlu <62553978+Spectac@users.noreply.github.com>
2023-08-09 02:16:40 +0300

    Add project files.

---
+++ LearningCenter.WhyYouShouldNotReThrowException/Human.cs
@@ -0,0 +1,19 @@
+﻿namespace LearningCenter.WhyYouShouldNotReThrowException
+{
+    public class Human
+    {
+
+        public void CheckNameLenght(string name)
+        {
+
+            try
+            {
+                ArgumentNullException.ThrowIfNullOrEmpty(name, nameof(name));
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ LearningCenter.WhyYouShouldNotReThrowException/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LearningCenter.WhyYouShouldNotReThrowException;
+
+try
+{
+    Human human = new Human();
+    human.CheckNameLenght(string.Empty);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.StackTrace.ToString());
+}
